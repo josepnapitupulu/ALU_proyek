cbf0ee933a027a24ad1126510fdb0edafb9cc733
josepnapitupulu <josepnapitupulu3@gmail.com>
2023-12-05 15:32:15 +0700

    Perubahan program

--- app.py
+++ app.py
@@ -5,20 +5,72 @@ from pso_algorithm import PSO
 app = Flask(__name__)
 
 koordinat_tempat_asal = {
-    "Asal_1": [5, 10],
-    "Asal_2": [5, 10],
-    "Asal_3": [5, 10],
-    "Asal_4": [5, 10],
+    "Balige Pusat Kantor": {
+        "Balige": [2.334655, 99.083166],
+        "Ajibata": [2.609101, 98.946025],
+        "Bonatua Lunasi": [2.525796, 99.115753],
+        "Borbor": [2.201029, 99.266624],
+        "Habinsaran": [2.310248, 99.336949],
+        "Laguboti": [2.3832665, 99.1486140],
+        "Lumban julu": [2.5767369, 99.0603619],
+        "Nassau": [2.2736008, 99.4038087],
+        "Parmaksian": [2.497967, 99.201461],
+        "Pintu Pohan Meranti": [2.553819, 99.304101],
+        "Porsea": [2.490482, 99.135669],
+        "Siantar Narumonda": [2.490482, 99.135669],
+        "Sigumpar": [2.399100, 99.199241],
+        "Silaen": [2.439718, 99.288264],
+        "Tampahan": [2.311486, 99.012720],
+        "Uluan": [2.462616, 99.070699],
+    },
 }
 
 koordinat_tempat_tujuan = {
-    "Kota_A": [20, 30],
-    "Kota_B": [40, 50],
-    "Kota_C": [60, 70],
-    "Kota_D": [80, 90],
-    "Kota_E": [5, 5],
-    "Kota_F": [39, 35],
-    "Kota_G": [20, 45],
+    "Ajibata": {
+        "Ajibata": [2.609101, 98.946025],
+    },
+    "Bonatua Lunasi": {
+        "Bonatua Lunasi": [2.525796, 99.115753],
+    },
+    "Borbor": {
+        "Borbor": [2.201029, 99.266624],
+    },
+    "Habinsaran": {
+        "Habinsaran": [2.310248, 99.336949],
+    },
+    "Laguboti": {
+        "Laguboti": [2.3832665, 99.1486140],
+    },
+    "Lumban julu": {
+        "Lumban julu": [2.5767369, 99.0603619],
+    },
+    "Nassau": {
+        "Nassau": [2.2736008, 99.4038087],
+    },
+    "Parmaksian": {
+        "Parmaksian": [2.497967, 99.201461],
+    },
+    "Pintu Pohan Meranti": {
+        "Pintu Pohan Meranti": [2.553819, 99.304101],
+    },
+    "Porsea": {
+        "Porsea": [2.490482, 99.135669],
+    },
+    "Siantar Narumonda": {
+        "Siantar Narumonda": [2.490482, 99.135669],
+    },
+    "Sigumpar": {
+        "Sigumpar": [2.399100, 99.199241],
+    },
+    "Silaen": {
+        "Silaen": [2.439718, 99.288264],
+    },
+    "Tampahan": {
+        "Tampahan": [2.311486, 99.012720],
+    },
+    "Uluan": {
+        "Uluan": [2.462616, 99.070699],
+    },
 }
 
 @app.route('/', methods=['GET', 'POST'])
@@ -32,14 +84,17 @@ def home():
         batas_bawah = [0, 0]
         batas_atas = [100, 100]
 
-        solusi_terbaik, jarak_terbaik, tempat_terdekat = PSO(
-            n_partikel, n_iterasi, batas_bawah, batas_atas, tempat_asal,
-            koordinat_tempat_asal, tempat_tujuan, koordinat_tempat_tujuan
+        lokasi_asal = request.form['lokasi_asal']
+        lokasi_tujuan = request.form['lokasi_tujuan']
+
+        solusi_terbaik, total_jarak, tempat_terdekat, rute_terbaik= PSO(
+            n_partikel, n_iterasi, batas_bawah, batas_atas,
+            koordinat_tempat_asal, koordinat_tempat_tujuan,
+            lokasi_asal, lokasi_tujuan
         )
 
-
         return render_template('index.html', solusi_terbaik=solusi_terbaik.tolist(),
-                               jarak_terbaik=jarak_terbaik, tempat_terdekat=tempat_terdekat)
+                               total_jarak=total_jarak, tempat_terdekat=tempat_terdekat, rute_terbaik=rute_terbaik)
 
     tempat_asal = list(koordinat_tempat_asal.keys())
     tempat_tujuan = list(koordinat_tempat_tujuan.keys())
--- pso_algorithm.py
+++ pso_algorithm.py
@@ -1,27 +1,42 @@
+# # pso_algorithm.py
+
 import numpy as np
 
 def hitung_jarak(x, y):
-    return np.sqrt(np.sum((x - y)**2))
+    if isinstance(x, dict):
+        x = np.array(list(x.values())[0])
+    if isinstance(y, dict):
+        y = np.array(list(y.values())[0])
 
-def PSO(n_partikel, n_iterasi, batas_bawah, batas_atas, tempat_asal, koordinat_tempat_asal, tempat_tujuan, koordinat_tempat_tujuan):
+    return round(np.sqrt(np.sum((x - y)**2)), 2)
+
+def PSO(n_partikel, n_iterasi, batas_bawah, batas_atas, koordinat_tempat_asal, koordinat_tempat_tujuan, lokasi_asal, lokasi_tujuan):
     dimensi = len(batas_bawah)
 
-    posisi_tempat_asal = np.array([koordinat_tempat_asal[tempat_asal[0]]])
+    if not isinstance(koordinat_tempat_asal, dict) or not isinstance(koordinat_tempat_tujuan, dict):
+        raise ValueError("koordinat_tempat_asal dan koordinat_tempat_tujuan harus berupa dictionary")
+
+    tempat_asal = list(koordinat_tempat_asal.keys())
+    tempat_tujuan = list(koordinat_tempat_tujuan.keys())
+
+    posisi_tempat_asal = np.array(koordinat_tempat_asal[lokasi_asal][list(koordinat_tempat_asal[lokasi_asal].keys())[0]])
 
     posisi_partikel = np.random.uniform(batas_bawah, batas_atas, size=(n_partikel, dimensi))
     kecepatan_partikel = np.random.rand(n_partikel, dimensi)
 
     pbest = posisi_partikel.copy()
-    fitness_pbest = np.array([hitung_jarak(pos, posisi_tempat_asal[0]) for pos in pbest])
+    fitness_pbest = np.array([hitung_jarak(pos, posisi_tempat_asal) for pos in pbest])
     gbest_index = np.argmin(fitness_pbest)
     gbest = pbest[gbest_index].copy()
 
     inertia_weight = 0.8
     c1 = 1.5
     c2 = 1.5
+    
+    rute_terbaik = []
 
     for iterasi in range(n_iterasi):
-        fitness_partikel = np.array([hitung_jarak(pos, posisi_tempat_asal[0]) for pos in posisi_partikel])
+        fitness_partikel = np.array([hitung_jarak(pos, posisi_tempat_asal) for pos in posisi_partikel])
 
         mask_update_pbest = fitness_partikel < fitness_pbest
         pbest[mask_update_pbest] = posisi_partikel[mask_update_pbest]
@@ -36,12 +51,114 @@ def PSO(n_partikel, n_iterasi, batas_bawah, batas_atas, tempat_asal, koordinat_t
             c1 * r1 * (pbest - posisi_partikel) +
             c2 * r2 * (gbest - posisi_partikel)
         )
+
+        # Perbaikan: Ambil kota-kota dari daerah yang dipilih
+        daerah_dipilih = [tempat_asal[int(index)] for index in gbest if 0 <= int(index) < len(tempat_asal)]
+        kota_dari_daerah_dipilih = []
+        for daerah in daerah_dipilih:
+            if isinstance(daerah, str):
+                # Perbaikan: Handle jika daerah adalah string (bukan dictionary)
+                kota_dari_daerah_dipilih.append(daerah)
+            else:
+                kota_dari_daerah_dipilih.extend(daerah['kota'])
+            
+        jumlah_kota_yang_diambil = min(len(kota_dari_daerah_dipilih), 3)
+        kota_yang_diambil = np.random.choice(kota_dari_daerah_dipilih, jumlah_kota_yang_diambil, replace=False)
+
+        # Perbaikan: Update rute_terbaik pada setiap iterasi
+        rute_terbaik = [lokasi_asal] + [list(koordinat_tempat_asal[lokasi_asal].keys())[0]]
+        for kota_index in kota_yang_diambil:
+            rute_terbaik.extend([kota_index] if isinstance(kota_index, str) else tempat_asal[kota_index]['kota'])
+
+        # Perbaikan: Hentikan iterasi jika kota tujuan sudah tercapai
+        if lokasi_tujuan in rute_terbaik:
+            break
+            
         posisi_partikel = posisi_partikel + kecepatan_partikel
         posisi_partikel = np.clip(posisi_partikel, batas_bawah, batas_atas)
 
-    tempat_terdekat_index = np.argmin([hitung_jarak(gbest, koordinat_tempat_tujuan[tempat]) for tempat in tempat_tujuan])
-    tempat_terdekat = list(tempat_tujuan)[tempat_terdekat_index]
+    tempat_terdekat_index = np.argmin([hitung_jarak(gbest, koordinat_tempat_tujuan[tempat][list(koordinat_tempat_tujuan[tempat].keys())[0]]) for tempat in tempat_tujuan])
+    tempat_terdekat = tempat_tujuan[tempat_terdekat_index]
 
-    total_jarak = hitung_jarak(gbest, koordinat_tempat_tujuan[tempat_terdekat])
+    total_jarak = hitung_jarak(gbest, koordinat_tempat_tujuan[tempat_terdekat][list(koordinat_tempat_tujuan[tempat_terdekat].keys())[0]])
 
-    return gbest, total_jarak, tempat_terdekat
+    return gbest, total_jarak, tempat_terdekat, rute_terbaik
+
+# 
+# # pso_algorithm.py
+
+# import numpy as np
+
+# def hitung_jarak(x, y):
+#     if isinstance(x, dict):
+#         x = np.array(list(x.values())[0])
+#     if isinstance(y, dict):
+#         y = np.array(list(y.values())[0])
+
+#     return round(np.sqrt(np.sum((x - y)**2)), 2)
+
+# def PSO(n_partikel, n_iterasi, batas_bawah, batas_atas, koordinat_tempat_asal, koordinat_tempat_tujuan, lokasi_asal, lokasi_tujuan):
+#     dimensi = len(batas_bawah)
+
+#     if not isinstance(koordinat_tempat_asal, dict) or not isinstance(koordinat_tempat_tujuan, dict):
+#         raise ValueError("koordinat_tempat_asal and koordinat_tempat_tujuan should be dictionaries")
+
+#     tempat_asal = list(koordinat_tempat_asal.keys())
+#     tempat_tujuan = list(koordinat_tempat_tujuan.keys())
+
+#     posisi_tempat_asal = np.array(koordinat_tempat_asal[lokasi_asal][list(koordinat_tempat_asal[lokasi_asal].keys())[0]])
+
+#     posisi_partikel = np.random.uniform(batas_bawah, batas_atas, size=(n_partikel, dimensi))
+#     kecepatan_partikel = np.random.rand(n_partikel, dimensi)
+
+#     pbest = posisi_partikel.copy()
+#     fitness_pbest = np.array([hitung_jarak(pos, posisi_tempat_asal) for pos in pbest])
+#     gbest_index = np.argmin(fitness_pbest)
+#     gbest = pbest[gbest_index].copy()
+
+#     inertia_weight = 0.8
+#     c1 = 1.5
+#     c2 = 1.5
+    
+#     rute_terbaik = []
+
+#     for iterasi in range(n_iterasi):
+#         fitness_partikel = np.array([hitung_jarak(pos, posisi_tempat_asal) for pos in posisi_partikel])
+
+#         mask_update_pbest = fitness_partikel < fitness_pbest
+#         pbest[mask_update_pbest] = posisi_partikel[mask_update_pbest]
+#         fitness_pbest[mask_update_pbest] = fitness_partikel[mask_update_pbest]
+
+#         gbest_index = np.argmin(fitness_pbest)
+#         gbest = pbest[gbest_index].copy()
+
+#         r1, r2 = np.random.rand(n_partikel, dimensi), np.random.rand(n_partikel, dimensi)
+#         kecepatan_partikel = (
+#             inertia_weight * kecepatan_partikel +
+#             c1 * r1 * (pbest - posisi_partikel) +
+#             c2 * r2 * (gbest - posisi_partikel)
+#         )
+#         daerah_dipilih = [tempat_asal[int(index)] for index in gbest if 0 <= int(index) < len(tempat_asal)]
+
+#         # Ambil kota-kota dari daerah yang dipilih
+#         kota_dari_daerah_dipilih = []
+#         for daerah in sorted(daerah_dipilih, key=lambda x: x['nilai']):
+#             kota_dari_daerah_dipilih.extend(daerah['kota'])
+            
+#         jumlah_kota_yang_diambil = min(len(kota_dari_daerah_dipilih), 3)  # Ganti dengan jumlah yang sesuai
+#         kota_yang_diambil = np.random.choice(kota_dari_daerah_dipilih, jumlah_kota_yang_diambil, replace=False)
+
+#         rute_terbaik = [lokasi_asal] + [list(koordinat_tempat_asal[lokasi_asal].keys())[0]]
+#         for kota_index in kota_yang_diambil:
+#             kota = tempat_asal[kota_index]['kota']
+#             rute_terbaik.extend(kota)
+            
+#         posisi_partikel = posisi_partikel + kecepatan_partikel
+#         posisi_partikel = np.clip(posisi_partikel, batas_bawah, batas_atas)
+
+#     tempat_terdekat_index = np.argmin([hitung_jarak(gbest, koordinat_tempat_tujuan[tempat][list(koordinat_tempat_tujuan[tempat].keys())[0]]) for tempat in tempat_tujuan])
+#     tempat_terdekat = tempat_tujuan[tempat_terdekat_index]
+
+#     total_jarak = hitung_jarak(gbest, koordinat_tempat_tujuan[tempat_terdekat][list(koordinat_tempat_tujuan[tempat_terdekat].keys())[0]])
+
+#     return gbest, total_jarak, tempat_terdekat, rute_terbaik
